f0437b76375dbd18f21e0c530e72e2908d447092
Mohanjith Sudirikku Hannadige <moha@mohanjith.net>
2016-02-02 00:55:39 +0200

    Improved BlogML importer

--- blogml-importer.php
+++ blogml-importer.php
@@ -10,8 +10,6 @@ Stable tag: 1.0
 License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 */
 
-require_once('XPath.class.php');
-
 /**
  * BlogML Importer
  *
@@ -43,6 +41,7 @@ if ( class_exists( 'WP_Importer' ) ) {
 class BlogML_Import {
 
 	var $xPath;
+	var $sXml;
 	var $posts;
 	var $posts_processed = array (); // Array of arrays. [[0] => XML fragment, [1] => New post ID]
     var $old_new_post_mapping = array (); // Key is old permalink URL, value is new permalink URL
@@ -91,10 +90,10 @@ class BlogML_Import {
 		//blogmlnames is an array with the names in the blogml import file
 		$pass = 'changeme';
 		if (!(in_array($author, $this->blogmlnames))) { //a new blogml author name is found
-			++ $this->j;
 			$this->blogmlnames[$this->j] = $author; //add that new blogml author name to an array
 			$user_id = username_exists($this->newauthornames[$this->j]); //check if the new author name defined by the user is a pre-existing wp user
 			if (!$user_id) { //banging my head against the desk now.
+				++ $this->j;
 				if ($this->newauthornames[$this->j] == 'left_blank') { //check if the user does not want to change the authorname
 					$user_id = wp_create_user($author, $pass);
 					$this->newauthornames[$this->j] = $author; //now we have a name, in the place of left_blank.
@@ -114,30 +113,35 @@ class BlogML_Import {
 	
 	function parse_blogml()
 	{
-		$sXml = file_get_contents($this->file);
-		$this->xPath = new XPath();
-		$this->xPath->importFromString($sXml);
-		
-		// Get authors
-		$result = $this->xPath->match("/blog/authors/author");
-		$countResult = count($result);
-		for ($i = 0; $i < $countResult; $i++)
-		{
-			$authorId = $this->xPath->getAttributes($result[$i], "id");
-			$this->authors[$i] = $authorId;
+		$this->sXml = simplexml_load_file($this->file);
+
+		foreach($this->sXml->getDocNamespaces() as $strPrefix => $strNamespace) {
+		    if( strlen($strPrefix) == 0 ) {
+		        $strPrefix="blogml"; //Assign an arbitrary namespace prefix.
+		    }
+    		$this->sXml->registerXPathNamespace($strPrefix, $strNamespace);
 		}
 		
-		// Get Categories
-		$result = $this->xPath->match("/blog/categories/category");
+		// Get authors
+		$result = $this->sXml->xpath("/blogml:blog/blogml:authors/blogml:author");
 		$countResult = count($result);
 		for ($i = 0; $i < $countResult; $i++)
 		{
-			$categoryId = $this->xPath->getAttributes($result[$i], "id");
-			$this->categories[$i] = $categoryId;
+			$attributes = $result[$i]->attributes();
+			$this->authors[$i] = $attributes['id'];
+		}
+
+		// Get Categories
+		$result = $this->sXml->xpath("/blogml:blog/blogml:categories/blogml:category");
+		$countResult = count($result);
+		for ($i = 0; $i < $countResult; $i++)
+		{
+			$attributes = $result[$i]->attributes();
+			$this->categories[$i] = $attributes['id'];
 		}		
 		
 		// Get posts
-		$this->posts = $this->xPath->match("/blog/posts/post");
+		$this->posts = $this->sXml->xpath("/blogml:blog/blogml:posts/blogml:post");
 	}
 
 	function get_authors_from_post() {
@@ -250,11 +254,12 @@ class BlogML_Import {
 
 		wp_import_cleanup($this->id);
 		
+		$upload_dir = wp_upload_dir();
 		// Write out a CSV file with URL mappings - this should persist beyond this import,
 		// so write it out to disk ourselves
 		if (count($this->old_new_post_mapping) > 0)
 		{
-			$output_filename = 'permalinkmap.csv';
+			$output_filename = $upload_dir['path'].'/permalinkmap.csv';
 		
 			// Delete old permalink file
 			if (file_exists($output_filename))
@@ -273,7 +278,7 @@ class BlogML_Import {
 			fwrite($fhandle, $csv_file_contents);
 			fclose($fhandle);
 			
-			echo '<a href="permalinkmap.csv">Click here to download a CSV file containing mappings from imported Permalinks to the new WordPress Permalinks</a><br />Note that this file is statically generated, it will need to be manually deleted.<br />';
+			echo '<a href="'.$upload_dir['url'].'/permalinkmap.csv">Click here to download a CSV file containing mappings from imported Permalinks to the new WordPress Permalinks</a><br />Note that this file is statically generated, it will need to be manually deleted.<br />';
 		}
 
 		echo '<h3>'.sprintf(__('All done.').' <a href="%s">'.__('Have fun!').'</a>', get_option('home')).'</h3>';
@@ -282,48 +287,55 @@ class BlogML_Import {
 	function process_post($post) {
 		global $wpdb;
 
-		$post_URL = $this->xPath->getAttributes($post, "post-url");
-  		if ( $post_URL && !empty($this->posts_processed[$post_URL][1]) ) // Processed already
+		$post->registerXPathNamespace('blogml', 'http://www.blogml.com/2006/09/BlogML');
+
+		$attributes = $post->attributes();
+		$post_URL = $attributes["post-url"].'';
+		echo $post_URL;
+  		if ( $post_URL && isset($this->posts_processed[$post_URL]) && isset($this->posts_processed[$post_URL][1]) && !empty($this->posts_processed[$post_URL][1]) ) // Processed already
 			return 0;
       
 		// There are only ever one of these
-		$post_title_node           = $this->xPath->match('title[1]', $post);
-		$post_title                = $wpdb->escape(str_replace(array ('<![CDATA[', ']]>'), '', $this->xPath->getData($post_title_node[0])));
-		$post_date                 = $this->getDate($this->xPath->getAttributes($post, "date-created"));
-		$post_date_gmt             = $this->getDate($this->xPath->getAttributes($post, "date-created"));
+		$post_title_node           = $post->xpath('blogml:title[1]');
+		$post_title                = str_replace(array ('<![CDATA[', ']]>'), '', $post_title_node[0]);
+		$post_date                 = $this->getDate($attributes["date-created"]);
+		$post_date_gmt             = $this->getDate($attributes["date-created"]);
 		$comment_status            = 'open'; // Not supported yet - hard-coded to "open"
 		$ping_status               = 'open'; // Not supported yet - hard-coded to "open"
-		$blogml_post_approved      = (bool)$this->xPath->getAttributes($post, "approved");
+		$blogml_post_approved      = (bool)$attributes["approved"];
 		$post_status               = ($blogml_post_approved) ? "publish" : "draft"; // hard-code to either publsished or draft (draft could be changed to "private")
-		$post_name_nodes           = $this->xPath->match('child::post-name[1]', $post);
-		$post_name                 = $wpdb->escape($this->xPath->getData(str_replace(array ('<![CDATA[', ']]>'), '',$post_name_nodes[0])));
+		$post_name_nodes           = $post->xpath('child::blogml:post-name[1]');
+		$post_name                 = str_replace(array ('<![CDATA[', ']]>'), '',$post_name_nodes[0]);
 		$post_parent               = '0'; // not supported
 		$menu_order                = '0'; // not supported
 		$post_type                 = 'post'; // only support posts now - could be changed to support posts and articles
 		$guid                      = $post_URL; // For now, it's the URL
-		$primary_post_author_nodes = $this->xPath->match('authors/author[1]', $post);
-		$post_author               = $wpdb->escape($this->xPath->getAttributes($primary_post_author_nodes[0], 'ref'));
-		
-		$contentNodes = $this->xPath->match("content[1]", $post);
-		$is_base64_encoded = $this->getBoolean($this->xPath->getAttributes($contentNodes[0], "base64"));
-		$post_content = $this->xPath->getData($contentNodes[0]);
+		$primary_post_author_nodes = $post->xpath('blogml:authors/blogml:author[1]');
+		$primary_post_author_nodes_0_attributes = $primary_post_author_nodes[0]->attributes();
+		$post_author               = $primary_post_author_nodes_0_attributes['ref'];
+
+		$contentNodes = $post->xpath("blogml:content[1]");
+		$contentNodes_attributes = $contentNodes[0]->attributes();
+		$is_base64_encoded = $this->getBoolean($contentNodes_attributes["base64"]);
+		$post_content = $contentNodes[0].'';
 		if ($is_base64_encoded) {
 			$post_content = base64_decode($post_content);
 		}
-		$post_content = $wpdb->escape(str_replace(array ('<![CDATA[', ']]>'), '',$post_content));
+		$post_content = str_replace(array ('<![CDATA[', ']]>'), '',$post_content);
 		
 		$categories = array ();
-		$cat_results = $this->xPath->match("categories/category", $post);
+		$cat_results = $post->xpath("blogml:categories/blogml:category");
 		$cat_count = count($cat_results);
 		for ($cat_index = 0; $cat_index < $cat_count; $cat_index++) {
-			$categories[$cat_index] = $wpdb->escape($this->xPath->getAttributes($cat_results[$cat_index], 'ref'));
+			$categories_attributes = $cat_results[$cat_index]->attributes();
+			$categories[$cat_index] = $categories_attributes['ref'];
 		}
 
 		$tags = array ();
-		$tag_results = $this->xPath->match("tags/tag", $post);
+		$tag_results = $post->xpath("blogml:tags/blogml:tag");
 		$tag_count = count($tag_results);
 		for ($tag_index = 0; $tag_index < $tag_count; $tag_index++) {
-			$tags[$tag_index] = $wpdb->escape($this->xPath->getAttributes($tag_results[$tag_index], 'ref'));
+			$tags[$tag_index] = $this->xPath->getAttributes($tag_results[$tag_index], 'ref');
 		}
 
 		if ($post_id = post_exists($post_title, '', '')) {
@@ -341,11 +353,12 @@ class BlogML_Import {
 			// Get the post permalink and associate with the old post URL
 			$post_permalink = get_permalink($post_id);
 			
-			$this->old_new_post_mapping[$post_URL] = $post_permalink;
+			if ($post_URL)
+				$this->old_new_post_mapping[$post_URL] = $post_permalink;
 
 			// Memorize old and new ID.
-			if ( $post_id && $post_URL && $this->posts_processed[$post_URL] )
-				$this->posts_processed[$post_URL][1] = $post_id; // New ID.
+			if ( $post_id && $post_URL )
+				$this->posts_processed[$post_URL] = array($post_id); // New ID.
 			
 			// Add categories.
 			if (count($categories) > 0) {
@@ -368,34 +381,39 @@ class BlogML_Import {
 			printf(' '.__('(%s tags)'), $tag_count);
 		}
 		// Now for comments
-		$commentsNodes = $this->xPath->match("comments/comment", $post);
+		$commentsNodes = $post->xpath("blogml:comments/blogml:comment");
 		$num_comments = 0;
-		if ( count($commentsNodes) > 0 ) { foreach ($commentsNodes as $comment) {
-			$comment_author       = $wpdb->escape($this->xPath->getAttributes($comment, 'user-name'));
-			$comment_author_email = $wpdb->escape($this->xPath->getAttributes($comment, 'user-email'));
-			$comment_author_IP    = ''; // Unsupported
-			$comment_author_url   = $wpdb->escape($this->xPath->getAttributes($comment, 'user-url'));
-			$comment_date         = $this->getDate($this->xPath->getAttributes($comment, 'date-created'));
-			$comment_date_gmt     = $this->getDate($this->xPath->getAttributes($comment, 'date-created'));
-			$commentContentNodes = $this->xPath->match("content[1]", $comment);
-			$is_comment_base64_encoded = $this->getBoolean($this->xPath->getAttributes($commentContentNodes[0], "base64"));
-			$comment_content = $this->xPath->getData($commentContentNodes[0]);
-			if ($is_comment_base64_encoded) {
-				$comment_content = base64_decode($comment_content);
-			}
-			$comment_content = $wpdb->escape(str_replace(array ('<![CDATA[', ']]>'), '',$comment_content));
-			$is_comment_approved  = (bool) $this->xPath->getAttributes($comment, 'approved');
-			$comment_approved     = ($is_comment_approved) ? '1' : '0';
-			$comment_type         = ''; // I can't tell what data this is looking for - 
-										// the wordpress export has it as empty, so we do too. :)
-			$comment_parent       = '0'; // we don't currently support parented comments
+		if ( count($commentsNodes) > 0 ) { 
+			foreach ($commentsNodes as $comment) {
+				$comment->registerXPathNamespace('blogml', 'http://www.blogml.com/2006/09/BlogML');
+				$comment_attributes = $comment->attributes();
+				$comment_author       = $comment_attributes['user-name'];
+				$comment_author_email = $comment_attributes['user-email'];
+				$comment_author_IP    = ''; // Unsupported
+				$comment_author_url   = $comment_attributes['user-url'];
+				$comment_date         = $this->getDate($comment_attributes['date-created']);
+				$comment_date_gmt     = $this->getDate($comment_attributes['date-created']);
+				$commentContentNodes = $comment->xpath("blogml:content[1]");
+				$commentContentNode_0_attributes = $commentContentNodes[0]->attributes();
+				$is_comment_base64_encoded = $this->getBoolean($commentContentNode_0_attributes["base64"]);
+				$comment_content = $commentContentNodes[0].'';
+				if ($is_comment_base64_encoded) {
+					$comment_content = base64_decode($comment_content);
+				}
+				$comment_content = str_replace(array ('<![CDATA[', ']]>'), '',$comment_content);
+				$is_comment_approved  = (bool) $comment_attributes['approved'];
+				$comment_approved     = ($is_comment_approved) ? '1' : '0';
+				$comment_type         = ''; // I can't tell what data this is looking for - 
+											// the wordpress export has it as empty, so we do too. :)
+				$comment_parent       = '0'; // we don't currently support parented comments
 
-			if ( !comment_exists($comment_author, $comment_date) ) {
-				$commentdata = compact('comment_post_ID', 'comment_author', 'comment_author_url', 'comment_author_email', 'comment_author_IP', 'comment_date', 'comment_date_gmt', 'comment_content', 'comment_approved', 'comment_type', 'comment_parent');
-				wp_insert_comment($commentdata);
-				$num_comments++;
+				if ( !comment_exists($comment_author, $comment_date) ) {
+					$commentdata = compact('comment_post_ID', 'comment_author', 'comment_author_url', 'comment_author_email', 'comment_author_IP', 'comment_date', 'comment_date_gmt', 'comment_content', 'comment_approved', 'comment_type', 'comment_parent');
+					wp_insert_comment($commentdata);
+					$num_comments++;
+				}
 			}
-		} }
+		}
 
 		if ( $num_comments )
 			printf(' '.__('(%s comments)'), $num_comments);
